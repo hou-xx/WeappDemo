97f1128c0a5c01831e9638cd09d5acfa2fb96ed1
houxx <hou_xiangxiang@126.com>
2017-03-17 22:41:41 +0800

    change style of homepage;add listPage.

---
+++ pages/secondary/listPage/listPage.js
@@ -0,0 +1,30 @@
+var appInstance = getApp();
+var TD = require('../../utils/tdweapp.js');
+Page({
+  data: {
+    text: "Page listPage"
+  },
+  onLoad: function (options) {
+    // 页面初始化 options为页面跳转所带来的参数
+    TD.Page.load();
+    TD.event({
+      id: 'listPage',
+      label: options.from,
+      params: {
+        count: 1
+      }
+    });
+  },
+  onReady: function () {
+    // 页面渲染完成
+  },
+  onShow: function () {
+    // 页面显示
+  },
+  onHide: function () {
+    // 页面隐藏
+  },
+  onUnload: function () {
+    // 页面关闭
+  }
+})
